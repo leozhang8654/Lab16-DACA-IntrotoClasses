e954cb23357480393e3a39686fa504a9d6e71f26
Leo Zhang <leo.zhang111@icloud.comm>
2026-07-28 22:16:34 -0700

    Lab 16: DACArecipient class with getters, setters, setAll

--- DACArecipient.java
+++ DACArecipient.java
@@ -1,33 +1,242 @@
+/*
+ * UML CLASS DIAGRAM:
+ * -----------------------------------------
+ * DACArecipient
+ * -----------------------------------------
+ * - surname : String
+ * - givenName : String
+ * - uscisNumber : String
+ * - countryOfOrigin : String
+ * - birthday : int
+ * - validFromDate : int
+ * - expirationDate : int
+ * - sex : char
+ * -----------------------------------------
+ * + getSurname() : String
+ * + setSurname(surname : String) : void
+ * + getGivenName() : String
+ * + setGivenName(givenName : String) : void
+ * + getUscisNumber() : String
+ * + setUscisNumber(uscisNumber : String) : void
+ * + getCountryOfOrigin() : String
+ * + setCountryOfOrigin(countryOfOrigin : String) : void
+ * + getBirthday() : int
+ * + setBirthday(birthday : int) : void
+ * + getValidFromDate() : int
+ * + setValidFromDate(validFromDate : int) : void
+ * + getExpirationDate() : int
+ * + setExpirationDate(expirationDate : int) : void
+ * + getSex() : char
+ * + setSex(sex : char) : void
+ * + setAll(surname : String, givenName : String, uscisNumber : String,
+ *          countryOfOrigin : String, birthday : int, validFromDate : int,
+ *          expirationDate : int, sex : char) : void
+ * -----------------------------------------
+ */
+
 /**
  * Represents one person receiving Deferred Action for Childhood Arrivals (DACA).
- * 
- * <TODO: add @author info here for all group-mates!>
  *
- *	@version 1.0
- **/
-
-//TODO: Complete UML class diagram
-/* UML CLASS DIAGRAM:
------------------------------------------
-<class name>
------------------------------------------
-<data, i.e. variables>
------------------------------------------
-<actions, i.e. methods>
------------------------------------------
-*/
-
+ * @author add your name here
+ * @version 1.0
+ */
 public class DACArecipient
 {
-	/***** INSTANCE VARIABLES *****/
-	//TODO: Declare the instance variables. 
+    // ===== INSTANCE VARIABLES =====
+    private String surname;
+    private String givenName;
+    private String uscisNumber;
+    private String countryOfOrigin;
+    private int birthday;
+    private int validFromDate;
+    private int expirationDate;
+    private char sex;
 
-	/***** ACCESSORS *****/
-	//TODO: Write the getter for each instance variable. Remember to include documentation for each method.
-	
-	/***** MUTATORS *****/
-	//TODO: Write the setter for each instance variable. Remember to include documentation for each method.
+    // ===== ACCESSORS =====
+    /**
+     * Gets the surname.
+     *
+     * @return the surname
+     */
+    public String getSurname()
+    {
+        return surname;
+    }
 
-	//TODO: Write the setAll method. Remember to include documentation.
+    /**
+     * Gets the given name.
+     *
+     * @return the given name
+     */
+    public String getGivenName()
+    {
+        return givenName;
+    }
 
+    /**
+     * Gets the USCIS number.
+     *
+     * @return the USCIS number
+     */
+    public String getUscisNumber()
+    {
+        return uscisNumber;
+    }
+
+    /**
+     * Gets the country of origin.
+     *
+     * @return the country of origin
+     */
+    public String getCountryOfOrigin()
+    {
+        return countryOfOrigin;
+    }
+
+    /**
+     * Gets the birthday (Julian Day Number).
+     *
+     * @return the birthday (Julian Day Number)
+     */
+    public int getBirthday()
+    {
+        return birthday;
+    }
+
+    /**
+     * Gets the valid-from date (Julian Day Number).
+     *
+     * @return the valid-from date (Julian Day Number)
+     */
+    public int getValidFromDate()
+    {
+        return validFromDate;
+    }
+
+    /**
+     * Gets the expiration date (Julian Day Number).
+     *
+     * @return the expiration date (Julian Day Number)
+     */
+    public int getExpirationDate()
+    {
+        return expirationDate;
+    }
+
+    /**
+     * Gets the sex.
+     *
+     * @return the sex
+     */
+    public char getSex()
+    {
+        return sex;
+    }
+
+    // ===== MUTATORS =====
+    /**
+     * Sets the surname.
+     *
+     * @param surname the surname
+     */
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    /**
+     * Sets the given name.
+     *
+     * @param givenName the given name
+     */
+    public void setGivenName(String givenName)
+    {
+        this.givenName = givenName;
+    }
+
+    /**
+     * Sets the USCIS number.
+     *
+     * @param uscisNumber the USCIS number
+     */
+    public void setUscisNumber(String uscisNumber)
+    {
+        this.uscisNumber = uscisNumber;
+    }
+
+    /**
+     * Sets the country of origin.
+     *
+     * @param countryOfOrigin the country of origin
+     */
+    public void setCountryOfOrigin(String countryOfOrigin)
+    {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    /**
+     * Sets the birthday (Julian Day Number).
+     *
+     * @param birthday the birthday (Julian Day Number)
+     */
+    public void setBirthday(int birthday)
+    {
+        this.birthday = birthday;
+    }
+
+    /**
+     * Sets the valid-from date (Julian Day Number).
+     *
+     * @param validFromDate the valid-from date (Julian Day Number)
+     */
+    public void setValidFromDate(int validFromDate)
+    {
+        this.validFromDate = validFromDate;
+    }
+
+    /**
+     * Sets the expiration date (Julian Day Number).
+     *
+     * @param expirationDate the expiration date (Julian Day Number)
+     */
+    public void setExpirationDate(int expirationDate)
+    {
+        this.expirationDate = expirationDate;
+    }
+
+    /**
+     * Sets the sex.
+     *
+     * @param sex the sex
+     */
+    public void setSex(char sex)
+    {
+        this.sex = sex;
+    }
+
+    /**
+     * Sets every instance variable at once.
+     *
+     * @param surname the surname
+     * @param givenName the given name
+     * @param uscisNumber the USCIS number
+     * @param countryOfOrigin the country of origin
+     * @param birthday the birthday (Julian Day Number)
+     * @param validFromDate the valid-from date (Julian Day Number)
+     * @param expirationDate the expiration date (Julian Day Number)
+     * @param sex the sex
+     */
+    public void setAll(String surname, String givenName, String uscisNumber,
+            String countryOfOrigin, int birthday, int validFromDate,
+            int expirationDate, char sex)
+    {
+        setSurname(surname);
+        setGivenName(givenName);
+        setUscisNumber(uscisNumber);
+        setCountryOfOrigin(countryOfOrigin);
+        setBirthday(birthday);
+        setValidFromDate(validFromDate);
+        setExpirationDate(expirationDate);
+        setSex(sex);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,65 @@
-/********************************************
-*	DEVELOPER:	<name>
-* COLLABORATORS: <names>
-*	LAST MODIFIED:	<date>
-********************************************/
-/********************************************
-*	DACArecipient Tester
-*********************************************
-*	PROGRAM DESCRIPTION:
-*	This program is to test methods of the DACArecipient class.
-*********************************************/
+/*
+ * DEVELOPER: <your name>
+ * COLLABORATORS: <anyone who helped>
+ * COURSE: CS 111 Intro to CS I - Java
+ * LAST MODIFIED: July 28, 2026
+ *
+ * DACArecipient Tester
+ * PROGRAM DESCRIPTION:
+ * Tests the DACArecipient class by creating two recipients (one built with
+ * individual setters, one with setAll) and printing their data.
+ */
 
-class Main 
+/**
+ * Tests the DACArecipient class by building two recipients and printing them.
+ */
+class Main
 {
-  public static void main(String[] args) 
-	{
-    //TODO PART 3: test your setter/getter here!
-  }
+    /**
+     * Creates two DACArecipient objects and prints their data.
+     *
+     * @param args the command-line arguments (not used)
+     */
+    public static void main(String[] args)
+    {
+        //First recipient: set each field with its own setter
+        DACArecipient person1 = new DACArecipient();
+        person1.setSurname("Mendez");
+        person1.setGivenName("Javier");
+        person1.setUscisNumber("56-3-445");
+        person1.setCountryOfOrigin("Venezuela");
+        person1.setBirthday(2451564);
+        person1.setValidFromDate(3956753);
+        person1.setExpirationDate(3956840);
+        person1.setSex('M');
+
+        //Second recipient: set everything at once with setAll
+        DACArecipient person2 = new DACArecipient();
+        person2.setAll("Kim", "Seo-yun", "25-6-567", "South Korea",
+                2451257, 39567322, 39567818, 'F');
+
+        printRecipient("Person 1", person1);
+        System.out.println();
+        printRecipient("Person 2", person2);
+    }
+
+    /**
+     * Prints all fields of one recipient under a heading.
+     *
+     * @param heading the label to show above the data
+     * @param person the recipient whose data is printed
+     */
+    public static void printRecipient(String heading, DACArecipient person)
+    {
+        System.out.println(heading);
+        System.out.println("-----------------------------");
+        System.out.println("Surname: " + person.getSurname());
+        System.out.println("Given name: " + person.getGivenName());
+        System.out.println("USCIS Number: " + person.getUscisNumber());
+        System.out.println("Country of Origin: " + person.getCountryOfOrigin());
+        System.out.println("Birthday(JDN): " + person.getBirthday());
+        System.out.println("Valid From Date(JDN): " + person.getValidFromDate());
+        System.out.println("Expiration Date(JDN): " + person.getExpirationDate());
+        System.out.println("Sex: " + person.getSex());
+    }
 }
